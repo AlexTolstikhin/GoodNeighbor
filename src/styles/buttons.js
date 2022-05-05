b56a0712c2d8d867427c214b02c8dcac9bab0470
otolstikhin <otolstikhin@sofi.org>
2022-05-04 21:07:09 -0700

    Fix button styles

--- src/styles/buttons.js
+++ src/styles/buttons.js
@@ -1,10 +1,11 @@
+import { Colors } from '.';
 const fullWidthRoundedButton = {
   alignItems: 'center',
   borderRadius: 30,
   marginHorizontal: 20,
   paddingVertical: 14,
   paddingHorizontal: 24,
-  borderColor: 'grey',
+  borderColor: Colors.pineGreen700,
   borderWidth: 2
 };
 
