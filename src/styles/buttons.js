99600d30f7d09e4f7cedaf4345ba757dc8f4861d
otolstikhin <otolstikhin@sofi.org>
2022-05-04 21:08:38 -0700

    Add correct border color

--- src/styles/buttons.js
+++ src/styles/buttons.js
@@ -1,11 +1,10 @@
-import { Colors } from '.';
 const fullWidthRoundedButton = {
   alignItems: 'center',
   borderRadius: 30,
   marginHorizontal: 20,
   paddingVertical: 14,
   paddingHorizontal: 24,
-  borderColor: Colors.pineGreen700,
+  borderColor: '#00796B',
   borderWidth: 2
 };
 
