210445928c10e2dc17a126af5bdae6a7815af7b7
otolstikhin <otolstikhin@sofi.org>
2022-05-04 21:05:26 -0700

    Clean up styles

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,33 +1,74 @@
 import React from 'react';
-import { Button, Image, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import RoundedButtonFullWidth from '../components/roundedButtonFullWidth';
 import HeroWelcomePageImage from '../assets/images/WelcomeHero.png';
 import useImageDimensions from '../hooks/useImageDimensions';
+import { bodyOne, headerOne } from '../styles/typography';
 
 const WelcomePage = () => {
   const { imageDimensions } = useImageDimensions();
   const onGetStartedClick = () => {};
   const onSignInClick = () => {};
+  const {
+    bodyText,
+    bottomSection,
+    buttonWrapper,
+    headerText,
+    mainContainer,
+    middleSection,
+    topSection,
+  } = styles;
   return (
-    <View>
-      <View>
+    <View style={mainContainer}>
+      <View style={topSection}>
         <Image
           source={HeroWelcomePageImage}
           style={imageDimensions}
         />
       </View>
-      <View>
-        <Text>Welcome to GoodNeighbor</Text>
-        <Text>
+      <View style={middleSection}>
+        <Text style={headerText}>Welcome to GoodNeighbor</Text>
+        <Text style={bodyText}>
           Making it easy for neighbors and co-workers to pickup food for each
           other, saving on delivery fees and helping local restaurants.
         </Text>
       </View>
-      <View>
-        <Button onPress={onGetStartedClick} title="Get Started" />
-        <Button onPress={onSignInClick} title="Sign In" />
+      <View style={bottomSection}>
+        <View style={buttonWrapper}>
+          <RoundedButtonFullWidth onPress={onGetStartedClick} label="Get Started" withBackground />
+        </View>
+        <View style={buttonWrapper}>
+          <RoundedButtonFullWidth onPress={onSignInClick} label="Sign In" />
+        </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  topSection: {
+    flex: 80,
+    justifyContent: 'center'
+  },
+  middleSection: {
+    flex: 20,
+    marginHorizontal: 20
+  },
+  bottomSection: {
+    flex: 25,
+  },
+  buttonWrapper: {
+    marginVertical: 5
+  },
+  headerText: {
+    ...headerOne
+  },
+  bodyText: {
+    ...bodyOne
+  }
+});
 
 export default WelcomePage;
